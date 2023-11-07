7204b7222d7c59c2a4925b3a76ed6f7c18052138
Motekema <Motekema@gmail.com>
2023-11-07 09:49:36 +0000

    My commit

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A modulse containing the State model"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Implements the State models for any state object"""
+    name = ""
